9d1c042f72386f2de39d5a934d4fa2e4678c2d74
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-12 10:34:51 +0200

    created all the components folders

---
+++ src/services/crytoApi.js
@@ -0,0 +1,9 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchange/-zdvbieRdZ',
+//     headers: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       'X-RapidAPI-Key': '4ce6b6f506mshb923b7c41dd5aecp175edfjsndf5568c2a6e5',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
---
+++ src/services/crytoNewsApi.jsx
@@ -0,0 +1,6 @@
+// headers: {
+//     'X-BingApis-SDK': 'true',
+//     'X-RapidAPI-Key': '4ce6b6f506mshb923b7c41dd5aecp175edfjsndf5568c2a6e5',
+//     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+//   }
+// };
